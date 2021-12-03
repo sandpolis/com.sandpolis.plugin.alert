30eb6dd2fcdef5134fe90518044148a8feda75a7
Tyler Cook <tcc@sandpolis.com>
2021-12-02 23:41:10 -0600

    Use dynamic junit version

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,8 @@ plugins {
 }
 
 dependencies {
-	testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
-	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.+")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.+")
 
 	//implementation platform("ai.djl:bom:0.13.0")
 	//implementation "ai.djl.mxnet:mxnet-engine"
