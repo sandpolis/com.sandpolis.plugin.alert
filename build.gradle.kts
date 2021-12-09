59d7462ce35b450b7df0e2580b2a2efb603fcfcb
Tyler Cook <tcc@sandpolis.com>
2021-12-08 19:58:08 -0600

    Update dependency paths

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
 	if (project.getParent() == null) {
 		api("com.sandpolis:core.instance:+")
 	} else {
-		api(project(":module:com.sandpolis.core.instance"))
+		api(project(":core:com.sandpolis.core.instance"))
 	}
 }
 
